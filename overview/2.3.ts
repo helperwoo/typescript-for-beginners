4d827242edf1af3dc168ed648dc2e8ad160f8f82
helperwoo <helperwoo.ko@gmail.com>
2025-03-04 14:35:20 +0900

    Types of TS part Three

---
+++ overview/2.3.ts
@@ -0,0 +1,11 @@
+// #2.3 Types of TS part Two
+
+// const numbers: number[] = [1, 2, 3, 4]
+// numbers.push(1)
+
+// readonly
+const numbers: readonly number[] = [1, 2, 3, 4]
+// numbers.push(1)
+
+// Tuple
+const player: [string, number, boolean] = ["nico", 1, true]
