00f1de469a04fd48f56ebd914312951a7a8114d6
helperwoo <helperwoo.ko@gmail.com>
2025-04-21 12:45:14 +0900

    #5.5 Blocks

--- typechain/src/index.ts
+++ typechain/src/index.ts
@@ -1,4 +1,23 @@
-import { init, exit } from "./myPackage";
+import crypto from 'crypto';
 
-init({ debug: true, url: 'true' });
-exit(0);
+interface BlcokShape {
+    hash: string
+    prevHash: string;
+    height: number;
+    data: string
+}
+
+class Block implements BlcokShape {
+    public hash: string;
+    constructor(
+        public prevHash: string,
+        public height: number,
+        public data: string
+    ) {
+        this.hash = Block.calculateHash(prevHash, height, data);
+    }
+
+    static calculateHash(prevHash: string, height: number, data: string) {
+        const toHash = `${prevHash}${height}${data}`;
+    }
+}
